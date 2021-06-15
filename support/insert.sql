d44a28f60989a430629adf994483bc6cc2c2a8fa
Cezary Uljasz <cezary.uljasz@student.wat.edu.pl>
2021-06-15 12:43:30 +0200

    Zadanie 3.1 – konfiguracja DB i uruchomienie aplikacji

--- support/insert.sql
+++ support/insert.sql
@@ -3,7 +3,7 @@ VALUES (1, 'Orson Scott Card'),
     (2, 'Robin Hobb'),
     (3, 'Alex Kava'),
     (4, 'Harlan Coben'),
-    (5, 'Megan Lindholm'),
+    (5, 'Megan Lindholm'), 
     (6, 'Kathryn Kidd');
 
 INSERT INTO Wydawnictwa
