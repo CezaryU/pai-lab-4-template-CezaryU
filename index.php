2ef4a4e32a9a45312571b9e712ba4d6bacdeb610
Cezary Uljasz <cezary.uljasz@student.wat.edu.pl>
2021-06-15 15:03:44 +0200

    Zadanie 4.8 – obsługa koszyka

--- index.php
+++ index.php
@@ -80,12 +80,31 @@ try {
       break;
     case 'addToBasket':
       //Dodawanie książki do koszyka
-      break;
+        switch ($portal->addToBasket()) { 
+          case INVALID_ID: 
+            case FORM_DATA_MISSING: 
+              $portal->setMessage("Błędny identyfikator książki."); 
+              break; 
+            case ACTION_OK: 
+              $portal->setMessage("Książka została dodana do koszyka."); 
+              break; 
+            default: 
+              $portal->setMessage("Błąd serwera."); 
+              break; 
+        } 
+        header("Location:index.php?action=showBasket"); 
+        break; 
+      case 'modifyBasket': // Modyfikacja zawartości koszyka 
+        $portal->setMessage("Zawartość koszyka została uaktualniona"); 
+        $portal->modifyBasket(); 
+        header("Location:index.php?action=showBasket"); 
+        break;
     case 'modifyBasket':
       //Modyfikacja zawartości koszyka
       break;
     case 'saveOrder':
       //Zapis zamówienia w bazie danych
+
       break;
     default:
       include 'templates/mainTemplate.php';
